a6cf6da294aca3760faeb5d7468042995009d051
josephmitchell48 <joemitch4848@gmail.com>
2023-09-10 11:51:27 -0400

    more debugging

--- src/hooks/usePrefersReducedMotion.js
+++ src/hooks/usePrefersReducedMotion.js
@@ -1,5 +1,5 @@
 export const usePrefersReducedMotion = () => {
-    if (!window.matchMedia) return false;
+    if (typeof window === 'undefined' || !window.matchMedia) return false;
 
     return window.matchMedia("(prefers-reduced-motion: reduce)").matches;
 };
